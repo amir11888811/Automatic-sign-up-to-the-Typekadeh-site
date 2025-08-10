0faf7c733a88ed52dbf578be52f3752cb8d61ae3
Amirhosseinmirzaei <amir13881188@gmail.com>
2025-08-10 19:53:27 +0330

    Automatic login to the Typekadeh site

---
+++ 1.py
@@ -0,0 +1,25 @@
+from selenium import webdriver
+from selenium.webdriver.common.by import By
+import time
+driver=webdriver.ChromiumEdge()
+driver.get("https://typekadeh.com")
+btn=driver.find_element(By.CLASS_NAME,"tk-btn")
+btn.click()
+time.sleep(4)
+btn=driver.find_elements(By.CLASS_NAME,"iranYekan-14--bold")
+if len(btn)>1:
+    btn=btn[1]
+    btn.click()
+time.sleep(3)
+input=driver.find_elements(By.CLASS_NAME,"tk-input__dense")
+time.sleep(1)
+input[1].send_keys("you'r email")
+time.sleep(3)
+input[2].send_keys("username")
+time.sleep(3)
+input[3].send_keys("password")
+time.sleep(3)
+btn=driver.find_elements(By.CLASS_NAME,"tk-btn--primary")
+print(len(btn))
+btn[3].click()
+time.sleep(200)
---
+++ readme.md
@@ -0,0 +1,9 @@
+## Automatic login to the Typekadeh site
+When we run this code, the system automatically opens the website, logs into the account, enters your Gmail, username, and password, and completes the registration.
+### instruction
+
+- first you write `cmd` in the Terminal
+- for install venv you must write `python -m venv .venv` in Terminal.
+- for active venv you must write `.venv\scripts\activate` in Terminal.
+- and you must install the library:`(pip install requests) and (pip install selenium)`
+- finally you must run the code `(python 1.py)`
