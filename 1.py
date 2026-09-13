c6959432e6e2da0f6ec4808eea7c529db79a2a98
AmirHossein Mirzaei <amir13881188@gmail.com>
2026-09-13 12:10:33 +0330

    Automatic sign up to the Typekadeh site updated

--- 1.py
+++ 1.py
@@ -9,3 +9,5 @@ def slow_type(element, text, delay=0.1):
         element.send_keys(char)
         time.sleep(delay)
 
+options = webdriver.EdgeOptions()
+options.add_experimental_option("detach", True)
