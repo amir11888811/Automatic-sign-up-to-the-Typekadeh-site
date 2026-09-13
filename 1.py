41e7a935b081eceb6d27badc25d0419d9fc66888
AmirHossein Mirzaei <amir13881188@gmail.com>
2026-09-13 12:20:03 +0330

    Automatic sign up to the Typekadeh site updated

--- 1.py
+++ 1.py
@@ -33,3 +33,6 @@ time.sleep(1.5)
 
 
 inputs = wait.until(EC.presence_of_all_elements_located((By.CLASS_NAME, "tk-input__dense")))
+
+slow_type(inputs[0], "your_email@gmail.com", delay=0.08)
+time.sleep(0.5)
