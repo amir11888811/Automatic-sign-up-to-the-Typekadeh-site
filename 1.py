a4a774dc3d34450f3a7051d094fe57ac2d2cb925
AmirHossein Mirzaei <amir13881188@gmail.com>
2026-09-13 12:09:21 +0330

    Automatic sign up to the Typekadeh site updated

--- 1.py
+++ 1.py
@@ -3,6 +3,11 @@ from selenium import webdriver
 from selenium.webdriver.common.by import By
 from selenium.webdriver.support.ui import WebDriverWait
 from selenium.webdriver.support import expected_conditions as EC
+def slow_type(element, text, delay=0.1):
+    element.clear()
+    for char in text:
+        element.send_keys(char)
+        time.sleep(delay)
 driver=webdriver.ChromiumEdge()
 driver.get("https://typekadeh.com")
 btn=driver.find_element(By.CLASS_NAME,"close-button")
